4c84e2758f5b688ada8209c01dd108172bdaf363
Jia Wu <cwu005104@gmail.com>
2025-03-25 23:38:06 +0800

    feat: 1. 區分程式碼conponents架構 2. 於vite.config.js設定mixin及alias

---
+++ src/components/Pomo.jsx
@@ -0,0 +1,64 @@
+import './Pomo.scss';
+
+import { useState, useEffect, useRef } from 'react';
+
+const Pomo=()=>{
+    const [ TimerStart, TimerStop, TimerReset ] = ["開始","停止","重置"]
+  // Timer Switch
+  const [timerSwitch, setTimerSwitch] = useState(false);
+  const switchTimer = () => {
+    const newTimerSwitch = !timerSwitch;
+    setTimerSwitch(newTimerSwitch);
+  };
+
+  // timing
+  const [timer, setTimer] = useState(0);
+  const intervalIdTimer = useRef();
+
+  //RefTimer可以計時但無法即時渲染畫面，可用於使用者流程計時器提升使用感
+  // const refTimer = useRef(0);
+
+  useEffect(() => {
+    if (timerSwitch) {
+      intervalIdTimer.current = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+        // refTimer.current = refTimer.current+1
+      }, 1000);
+    } else {
+      clearInterval(intervalIdTimer.current);
+    }
+
+    // destroy
+    return () => clearInterval(intervalIdTimer.current);
+  }, [timerSwitch]);
+
+  const resetTimer = () => {
+    setTimer(0);
+    clearInterval(intervalIdTimer.current);
+    setTimerSwitch(false);
+  };
+
+  return (
+    <div className="timer">
+      <div className="timer__wrapper">
+        <div className="timer__display">{timer}</div>
+        <div className="timer__btn">
+          <button
+            className="timer__btn timer__btn--switch"
+            onClick={switchTimer}
+          >
+            {timerSwitch ? TimerStop : TimerStart}
+          </button>
+          <button
+            className="timer__btn timer__btn--reset"
+            onClick={resetTimer}
+          >
+            {TimerReset}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pomo;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react'
+import path from'path'
 
+// 取得 ESM 環境下的 `__dirname`
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(()=>{
+
+  return{
   plugins: [react()],
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src/'),
+      '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+        },
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:`
+        @use '@styles/Mixin.scss' as *;`
+      }
+    }
+  }
+}
 })
