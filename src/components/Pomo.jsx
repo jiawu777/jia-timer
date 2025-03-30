ee0054f9de332f65b943c38e25d5fb217236ab42
Jia Wu <cwu005104@gmail.com>
2025-03-30 18:31:02 +0800

    feat/transtopomodoro Jia: 1. 新增 Task List (但跑版)

--- src/components/Pomo.jsx
+++ src/components/Pomo.jsx
@@ -3,8 +3,10 @@ import { useState, useEffect, useRef } from 'react';
 import './Pomo.scss';
 
 const Pomo = () => {
-  const [TimerStart, TimerStop] = ['Start', 'Pause'];
-
+  // Config
+  const [TimerStart, TimerStop, TimerSkip] = ['Start', 'Pause','Skip'];
+  const [CircularSize,CircularStrokeWidth] = [400, 50] 
+  
   // Timer Switch
   const [timerSwitch, setTimerSwitch] = useState(false);
   
@@ -16,6 +18,25 @@ const Pomo = () => {
   const timeInputRef = useRef(null)
   const taskInputRef = useRef(null)
   const defaultTime = 25
+
+  // get UserInfo
+  const userInfoRef = useRef(
+    localStorage.getItem('userInfo')?JSON.parse(localStorage.getItem('userInfo')):{ tasks: [], currentTaskId: null }
+  )
+  
+  // get current task
+  const currentTaskIndex = userInfoRef.current?.tasks?.findIndex(
+    (task) => task.id === userInfoRef.current?.currentTaskId
+  ) ?? -1;
+
+  const currentTask = currentTaskIndex >= 0 ? 
+  userInfoRef.current.tasks[currentTaskIndex] : null;
+
+  useEffect(()=>{
+    if(currentTask){
+      setTimer(currentTask.time)
+    }
+  },[currentTask])
 
   // timer Switch func
   const switchTimer = () => {
@@ -40,22 +61,54 @@ const Pomo = () => {
   // skip timer  
   const resetTimer = () => {
     clearInterval(intervalIdTimer.current);
-    let secInput = (timeInputRef.current.value)*60
-    setTimer(secInput);
+    if(currentTask){
+      setTimer(currentTask.time)
+    }
     setTimerSwitch(false);
   };
 
   // input time & task
   const handleTaskInput = (e) => {
+    // prevent default render
     e.preventDefault();
     clearInterval(intervalIdTimer.current);
 
-    let secInput = (timeInputRef.current.value)*60
-    
+    const secInput = Number(timeInputRef.current.value)*60;
+    const taskName = taskInputRef.current.value.trim();
 
+    if(!taskName) return
+
+    const newTask = {
+      id: userInfoRef.current.tasks.length+1,
+      name: taskName,
+      time: secInput
+    }
+
+    userInfoRef.current.tasks.push(newTask);
+    userInfoRef.current.currentTaskId = newTask.id;
     setTimer(secInput);
     setTimerSwitch(false);
+
+    localStorage.setItem('userInfo',JSON.stringify(userInfoRef.current));
+
+    taskInputRef.current.value = "";
   };
+
+  // Task List
+  const forMapTasks = (item) => {
+      return(
+        <li key={`${item.id}`} className="pomo__taskListLI" >
+          <label className="pomo__taskListItem" for="pomo__taskListItem"> 
+            <input className="pomo__taskListItem pomo__taskListItem--radio" type="radio" name="listGroupRadio" value="" id="" checked/>
+            <div className="pomo__taskListItem pomo__taskListItem--time">{item.time/60}min</div>
+            <div className="pomo__taskListItem pomo__taskListItem--task">{item.name}</div>
+            <button className="pomo__taskListItem pomo__taskListItem--closeBtn" type="button" class="btn-close" aria-label="Close"></button>
+            </label>
+        </li>
+      )
+  }
+
+const child = userInfoRef.current?.tasks?.map(forMapTasks) ?? [];
 
   return (
     <div className="pomo__wrapper">
@@ -63,11 +116,11 @@ const Pomo = () => {
         <CircularProgress
           className="pomo__circular"
           timer={timer}
-          timeInput={timeInputRef.current}
-          size={400}
-          strokeWidth={50}
+          timeInput={currentTask?.time||1500}
+          size={CircularSize}
+          strokeWidth={CircularStrokeWidth}
         ></CircularProgress>
-        <h2 className="pomo__task">{taskInputRef.current?.value || 'Time to focus!'}</h2>
+        <h2 className="pomo__currentTaskName">{currentTask?.name || 'Time to focus!'}</h2>
       </div>
 
       <div className="pomo__btn">
@@ -82,25 +135,29 @@ const Pomo = () => {
           className={`pomo__btn ${timerSwitch ? 'pomo__btn--skip' : 'pomo__btn--hide'}`}
           onClick={resetTimer}
         >
-          SKIP
+          {TimerSkip}
         </button>
+
       </div>
 
       <div className="pomo__input">
+      <h2 className="pomo__taskListTitle"> Task List </h2>
         <form className="pomo__form">
           <input
-            className="pomo__form pomo__form--time"
+            className="pomo__input pomo__input--time"
             type="number"
             ref={timeInputRef}
             defaultValue={defaultTime}
             disabled={timerSwitch}
+            required
           />
           <input
-            className="pomo__form pomo__form--task"
+            className="pomo__input pomo__input--task"
             type="text"
             ref={taskInputRef}
             placeholder="Enter task"
             disabled={timerSwitch}
+            required
           />
           <button
             onClick={handleTaskInput}
@@ -109,6 +166,12 @@ const Pomo = () => {
             Add task
           </button>
         </form>
+        
+        <div className="pomo__taskList">
+            <ul className="pomo__taskListUL">
+              {child}
+            </ul>
+          </div>
       </div>
     </div>
   );
